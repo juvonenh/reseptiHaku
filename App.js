f97bb5bdde8ba126170c3d5aaca95ac33ae60edd
Hanna Juvonen <hanna.juvonen@myy.haaga-helia.fi>
2024-09-14 20:04:32 +0300

    Tehtavan palautus

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  Image,
+} from "react-native";
 
 export default function App() {
+  const [keyword, setKeyword] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const handleFetch = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(
+            `Error in fetch: status code ${response.status} ${response.statusText}`
+          );
+
+        return response.json();
+      })
+      .then((data) => setRecipes(data.meals))
+      .catch((err) => console.error(err));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.text}
+          placeholder="Ingredient..."
+          value={keyword}
+          onChangeText={(text) => setKeyword(text)}
+        />
+        <Button title="FIND" onPress={handleFetch} />
+      </View>
+
+      <FlatList
+        data={recipes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={styles.text}>{item.strMeal}</Text>
+            <Image source={{ uri: item.strMealThumb }} style={styles.image} />
+          </View>
+        )}
+        ItemSeparatorComponent={<View style={styles.separator}></View>}
+      />
     </View>
   );
 }
@@ -13,8 +58,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  searchContainer: { width: 200, marginTop: 100, marginBottom: 20 },
+  text: { fontSize: 18 },
+  image: { width: 60, height: 60, marginBottom: 5 },
+  separator: {
+    height: 1,
+    width: "80%",
+    backgroundColor: "#CED0CE",
+    marginLeft: "10%",
   },
 });
